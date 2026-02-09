ff255f24cea5e25073edf26c5de8fc8bb78e3894
m-iwatani <m-iwatani@gvtech.co.jp>
2026-02-09 17:15:32 +0900

    ログイン画面のClassタイポ修正

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -21,7 +21,7 @@ export default function LoginPage() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-slate-50 px-4">
-      <div className="w-full max-w-100` space-y-8">
+      <div className="w-full max-w-md space-y-8">
         
 
 
