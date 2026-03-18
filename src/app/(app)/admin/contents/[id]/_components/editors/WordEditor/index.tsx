cf37ab373f53c7f7091d3daf0e4689b1d63c6fc7
m-iwatani <m-iwatani@gvtech.co.jp>
2026-03-18 10:14:35 +0900

    ビルドバグ対応

--- src/app/(app)/admin/contents/[id]/_components/editors/WordEditor/index.tsx
+++ src/app/(app)/admin/contents/[id]/_components/editors/WordEditor/index.tsx
@@ -31,7 +31,6 @@ export function WordEditor({ contentId, selectedWordId }: WordEditorProps) {
         >
           <WordList 
             contentId={contentId} 
-            selectedWordId={selectedWordId} 
           />
         </ResizablePanel>
 
